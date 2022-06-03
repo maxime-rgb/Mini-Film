b956023db8708ae9269a7d3d5b81e313cfb4d704
maxime-rgb <Maximethomas404@gmail.com>
2022-06-03 15:33:15 +0200

    Tp repo

--- main.js
+++ main.js
@@ -1,49 +1,66 @@
 let cardMovie = document.getElementById("film-content");
 
-async function getMovies() {
-    const url = "https://www.omdbapi.com/?s=jurassic_park&plot=short&apikey=2a4831ba"
+async function getMovies(searchValue = "jurassic_park") {
+    const url = "https://www.omdbapi.com/?s=" + searchValue + "&plot=short&apikey=2a4831ba"
     const response = await fetch(url)
     const data = await response.json()
     return data.Search
 }
 
 
-const data = getMovies().then(function(movies) {
-    console.log(movies)
+function generateMoviesList(searchValue) {
+    getMovies(searchValue).then(function(movies) {
+        console.log(movies)
 
-    movies.forEach(function(movie) {
-        const card = document.createElement("div")
-        card.classList.add("card")
+        movies.forEach(function(movie) {
+            const card = document.createElement("div")
+            card.classList.add("card")
 
-        const img = document.createElement("img")
-        img.setAttribute("src", movie.Poster)
-        img.classList.add("card-img-top")
-        card.appendChild(img)
+            const img = document.createElement("img")
+            img.setAttribute("src", movie.Poster)
+            img.classList.add("card-img-top")
+            card.appendChild(img)
 
-        const cardBody = document.createElement("div")
-        cardBody.classList.add("card-body")
-        card.appendChild(cardBody)
+            const cardBody = document.createElement("div")
+            cardBody.classList.add("card-body")
+            card.appendChild(cardBody)
 
-        const cardTitle = document.createElement("h5")
-        cardTitle.classList.add("card-title")
-        cardTitle.innerHTML = "<h5>Titre principal :</h5> " + movie.Title
-        cardBody.appendChild(cardTitle)
+            const cardTitle = document.createElement("h5")
+            cardTitle.classList.add("card-title")
+            cardTitle.innerHTML = "<h5>Titre principal :</h5> " + movie.Title
+            cardBody.appendChild(cardTitle)
 
-        const cardText = document.createElement("p")
-        cardText.classList.add("card-text")
-        cardText.textContent = "Année : " + movie.Year
-        cardBody.appendChild(cardText)
+            const cardText = document.createElement("p")
+            cardText.classList.add("card-text")
+            cardText.textContent = "Année : " + movie.Year
+            cardBody.appendChild(cardText)
 
-        const cardButton = document.createElement("a")
-        cardButton.classList.add("card-button")
-        cardButton.setAttribute('href', './Erreur404.html');
-        cardButton.innerHTML = '<button type="button" class="btn btn-outline-light">Voir plus</button>'
-        cardBody.appendChild(cardButton)
+            const cardButton = document.createElement("a")
+            cardButton.classList.add("card-button")
+            cardButton.setAttribute('href', "https://www.imdb.com/title/" + movie.imdbID);
+            cardButton.setAttribute("target", "_blank")
+            cardButton.innerHTML = '<button type="button" class="btn btn-outline-light">Voir plus</button>'
+            cardBody.appendChild(cardButton)
 
 
 
-        cardMovie.appendChild(card)
-
+            document.querySelector("#film-content").appendChild(card)
+        })
     })
+}
 
+////////////////////////
+// Execution du code au chargement de la page
+
+// Au chargement, on génère la liste par défaut avec recherche star wars
+generateMoviesList()
+
+// Au clic sur le bouton, on envoie la recherche tapée à la même fonction
+// Après avoir vidé le HTML
+
+document.querySelector("button").addEventListener("click", function() {
+    const searchValue = document.querySelector(".form-control").value
+
+    document.querySelector("#film-content").innerHTML = " "
+    generateMoviesList(searchValue)
 })
